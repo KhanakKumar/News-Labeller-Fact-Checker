8f42677c87e34d228063a62197f0d42438d3a9e9
KhanakKumar <khanakkumar3000@gmail.com>
2025-03-03 03:30:32 +0530

    connection BE-FE

---
+++ client/pages/check.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState} from 'react'
+
+function check () {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+    const response = await fetch("http://localhost:8080/api/home", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query: inputValue }),
+    });
+
+    const data = await response.json();
+    console.log("Backend Response:", data);
+  };
+
+  return (
+    <main className="flex h-screen items-center">
+        <form onSubmit={handleSubmit} className="w-screen mx-[110px]">
+            <div className="flex h-[80px] bg-white rounded-[20px] drop-shadow-[0_0_10px_rgba(0,0,0,0.50)] items-center">
+            <input
+                    id="username"
+                    name="username"
+                    type="text"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    placeholder='Enter Url'
+                    className="w-full h-[50px] px-[50px] text-gray-900 placeholder:text-gray-400 focus:outline-none text-4xl"
+            />
+            <button type='submit' className="w-1/6 h-[80px] rounded-[20px] bg-blue-500 hover:bg-blue-300 text-blue-50 text-4xl">Check</button>
+            </div>
+        </form>
+    </main>
+  )
+};
+export default check
--- server/server.js
+++ server/server.js
@@ -1,11 +1,28 @@
-const express = require("express");
+const express = require ("express")
+const cors = require("cors");
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
-app.get("/api/home" , (req, res) => {
-    res.json({message: "Hello World"});
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/home", (req, res) => {
+    res.json({ message: "Helloooo" });
+});
+
+app.post("/api/home" , (req, res) => {
+    console.log("Received request:", req.body);
+    const { query } = req.body;
+
+    if(!query){
+        console.log("Error: Query is missing!");
+        return res.status(400).json({ error: "Query is required" });
+    }
+
+    console.log("Received query:", query);
+    res.status(200).json({ message: "Query received", query });
 });
 
 app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
-})
+});
